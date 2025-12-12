96222520a1197774ee9e8874608463ab5baff93c
Keylan King <kingkey0101@outlook.com>
2025-12-12 12:19:25 -0600

    breadthFirstSearch

--- questions/binaryTrees/breadthFirstSearch/index.js
+++ questions/binaryTrees/breadthFirstSearch/index.js
@@ -1,13 +1,28 @@
 /** Breadth First Search **
- * 
- * Return the breadth first values of the given binary tree in 
- * an array.  
- * 
+ *
+ * Return the breadth first values of the given binary tree in
+ * an array.
+ *
  */
 
- const breadthFirstSearch = (root) => {
+const breadthFirstSearch = (root) => {
+  const queue = [root];
+  const result = [];
 
-}
+  //loop until queue is empty:
+  while (queue.length > 0) {
+    // remove first element from array and return it
+    const curr = queue.shift();
+    // add the curr node value to result
+    result.push(curr.val);
 
+    // check if curr node has children(left), if so push to queue;
+    if (curr.left !== null) queue.push(curr.left);
+    // check if curr node has children(right), if so push to queue;
+    if (curr.right !== null) queue.push(curr.right);
+  }
+
+  return result;
+};
 
 module.exports = breadthFirstSearch;
