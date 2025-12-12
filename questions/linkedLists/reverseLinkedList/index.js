aa43e593aee3c383d21f31bac51bc3fbaa3032b0
Keylan King <kingkey0101@outlook.com>
2025-12-11 23:07:13 -0600

    reverseLinkedList

--- questions/linkedLists/reverseLinkedList/index.js
+++ questions/linkedLists/reverseLinkedList/index.js
@@ -10,7 +10,16 @@
  */
 
  const reverse = (head) => {
-  
+  let prev = null
+  let curr = head
+
+  while(curr){
+    let next = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+  }
+  return prev
 }
 
 
