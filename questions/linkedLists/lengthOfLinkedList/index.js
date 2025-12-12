c105bb9d672efc194278972307214dc2cac4b221
Keylan King <kingkey0101@outlook.com>
2025-12-11 21:58:01 -0600

    lengthOfLinkedList

--- questions/linkedLists/lengthOfLinkedList/index.js
+++ questions/linkedLists/lengthOfLinkedList/index.js
@@ -1,18 +1,22 @@
 /** Length of Linked List **
- * 
- * Find the length of the given linked list. 
- *  
+ *
+ * Find the length of the given linked list.
+ *
  * @example
  * lengthOfList(1 -> 2 -> 3) -> 3
  * lengthOfList(10 -> 20) -> 2
  * lengthOfList(100 -> 200 -> 300 -> 400) -> 4
- * 
+ *
  */
 
- const lengthOfList = (head) => {
-
-
-}
-
+const lengthOfList = (head) => {
+  let lenght = 0;
+  let curr = head;
+  while (curr) {
+    lenght++;
+    curr = curr.next;
+  }
+  return lenght;
+};
 
 module.exports = lengthOfList;
